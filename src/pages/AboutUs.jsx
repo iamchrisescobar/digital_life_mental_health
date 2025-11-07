3424880a6f77b18acaa559ce353de9f8ad3c4398
Christian Escobar <chris_cobar@outlook.com>
2025-11-07 12:19:32 -0800

    added all team members information to AboutUs page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -61,60 +61,60 @@ export default function AboutUs() {
 
         <div className="team-grid">
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Senior · Psychology"
-            role="Research Lead"
+            name="Marina Nasr"
+            yearMajor="Third-year Biology major"
+            role="Project Narrative Lead"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="alex@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/marina-nasr-059a87288"
+            email="marinanasr@ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Junior · Data Science"
-            role="Data Analyst"
+            name=" Tina (Tianyi) Su"
+            yearMajor="Fourth-year Statistics and Data Science major, Global Health minor"
+            role="Data Visualization"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="taylor@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/tina-su-7a9b6c/"
+            email="kristinas7796@ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Sophomore · Sociology"
-            role="Writer and Editor"
+            name="Christian Escobar"
+            yearMajor="Fourth-year Computer Science major"
+            role="Web Development Manager"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="jordan@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/christianescobar02/"
+            email="chrisescobar@ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Senior · Design Media Arts"
-            role="Designer"
+            name="Leo Wong"
+            yearMajor="Third-year Statistics and Data Science major"
+            role="Data Specialist"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="morgan@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/leowuniv"
+            email="leowuniv@ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Junior · Statistics"
-            role="Methods"
+            name="Jiahong Chen"
+            yearMajor="Second-year Statistics and Data Science"
+            role="Data Visualization"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="casey@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/jiahong-chen-760139331/"
+            email="jiahongchen13@g.ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Sophomore · Communications"
-            role="Outreach"
+            name="Cynthia Szeto"
+            yearMajor="Fourth-year Psychology major Digital Humanities minor"
+            role="Project Manager"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="riley@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/cynthiaszeto/"
+            email="cynthiaszeto@ucla.edu"
           />
           <TeamMemberCard
-            name="Name Lastname"
-            yearMajor="Senior · Computer Science"
-            role="Web Developer"
+            name="Qiyang (Rosie) Chen"
+            yearMajor="Fourth-year double major in Statistics and Data Science, Applied Math, Specialization in Computing"
+            role="Data Visualization"
             imgSrc="https://placehold.co/320x320/png?text=Headshot"
-            linkedinUrl="#"
-            email="sam@example.edu"
+            linkedinUrl="https://www.linkedin.com/in/qiyang-rosalinda-chen-5b5584210/"
+            email="rosiechen1005@g.ucla.edu"
           />
         </div>
       </section>
